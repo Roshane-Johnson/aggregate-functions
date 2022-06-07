68da14d49afdd1f7e363f72f766d215d0c2a20f9
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-06-07 10:36:30 -0500

    initial commithttps://github.com/Roshane-Johnson/aggregate-functions.git

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const DB = require('../lib/db')
+
+router.get('/', (req, res) => {
+	res.render('index', { page_title: 'Home' })
+})
+
+module.exports = router
